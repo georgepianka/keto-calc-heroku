229f51d51687b561f75f906b97befdb6c1683aaf
NotoriousCottonball <gpianka1@gmail.com>
2019-09-11 08:18:30 -0700

    Update events.js to test EntryCalendar display

--- client/src/events.js
+++ client/src/events.js
@@ -1,16 +1,21 @@
 import moment from "moment";
 
 export const events = [
-  {
-    title: "All Day Event very long title",
+  { title: "NetCarbs: 20",
     allDay: true,
     start: new Date(moment()),
     end: new Date(moment())
   },
-  {
-    title: "02:00-06:59",
-    start: "2018-03-03 02:00",
-    end: "2018-03-03 06:59",
-    up_down_ind: "N"
-  }
+
+  { title: "Calories: 1500",
+    allDay: true,
+    start: new Date(moment()),
+    end: new Date(moment())
+  },
+
+  { title: "Weight: 190",
+    allDay: true,
+    start: new Date(moment()),
+    end: new Date(moment())
+  },
 ]
