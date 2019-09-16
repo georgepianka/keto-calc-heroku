2618612c386a92264dd0b21e9eb8e8f20f2929d9
NotoriousCottonball <gpianka1@gmail.com>
2019-09-15 21:30:05 -0700

    Finish Entry model
    With Validations

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,9 @@
 class Entry < ApplicationRecord
   belongs_to :user
+  validates_presence_of :date
+  validates_presence_of :calories
+  validates_presence_of :net_carbs
+  validates_presence_of :weight
+  # Some Entries will not have a value for :protein / :fat attributes
+
 end
