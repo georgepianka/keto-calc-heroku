9aeee9b19eaf89fec7fd3316e097ca7864c90d29
NotoriousCottonball <gpianka1@gmail.com>
2019-09-15 21:24:34 -0700

    Finish User model
    Include a find_or_create method for instsantiating a user object from a facebook authorization hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :entries
+  validates_presence_of :username, :email
+  validates_uniqueness_of :username, :email
+
+
+      #def self.find_or_create_from_facebook(auth_hash)
+    #      self.where(email: auth_hash["info"]["email"]).first_or_create do |u|
+    #        u.username = auth_hash.info.name
+    #        u.password = SecureRandom.hex
+    #      end
+    #  end
+
+
+    def self.find_or_create_from_facebook(authorization)
+          self.where(email: authorization['email']).first_or_create do |u|
+            u.username = authorization['name']
+            u.password = SecureRandom.hex
+          end
+    end
+
 end
