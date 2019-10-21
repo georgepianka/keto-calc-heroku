22e258d18ac833fc34420442ddb0f4e024d4ed5a
NotoriousCottonball <gpianka1@gmail.com>
2019-10-20 21:44:50 -0700

    Add FacebookLoginButton component

---
+++ client/src/components/FacebookLoginButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FacebookLoginBtn = () =>
+
+
+    // post login behavior should be defined at a higher level
+(
+      <div className="fb-login-button" data-width="500" data-size="large" data-button-type="login_with" data-auto-logout-link="false" data-use-continue-as="true" data-scope="public_profile, email"></div>
+    )
+
+
+export default FacebookLoginBtn;
