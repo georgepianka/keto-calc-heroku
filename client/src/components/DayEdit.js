ce81555bccb6187b29961659b84066f393ff49de
NotoriousCottonball <gpianka1@gmail.com>
2019-12-09 07:27:04 -0800

    Formatted DayEdit component

--- client/src/components/DayEdit.js
+++ client/src/components/DayEdit.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import { dayForm } from '../actions/dayForm';
 import { updateDayEditForm } from '../actions/dayForm';
 import { updateDay } from '../actions/currentDay';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, FormText, ListGroup, ListGroupItem, Badge } from 'reactstrap';
 import ketoCalc from '../styles/ketocalc.jpg';
 
 
@@ -28,6 +28,7 @@ const DayEdit = ( {currentDay, dayEditFormData, updateDayEditForm, updateDay, hi
 
 
   return (
+    <div>
       <Form className="mb-3" onSubmit={handleSubmit}>
        <FormGroup>
        <Label for="date"> Date </Label>
@@ -41,8 +42,33 @@ const DayEdit = ( {currentDay, dayEditFormData, updateDayEditForm, updateDay, hi
        <Button className="btn-lg" color="success">UPDATE</Button>
 
        </Form>
-  )
+
+
+
+
+
+
+  {/*
+         {currentDay.included.length > 0 ?
+           currentDay.included.map(entry => (
+           <div>
+             <h3> {entry.attributes.name} </h3>
+             <ListGroup>
+             <ListGroupItem className="justify-content-between">Net Carbs: <Badge pill>{entry.attributes.net_carbs}</Badge></ListGroupItem>
+             <ListGroupItem className="justify-content-between">Calories: <Badge pill>{entry.attributes.calories}</Badge></ListGroupItem>
+             <ListGroupItem className="justify-content-between">Protein: <Badge pill>{entry.attributes.protein}</Badge></ListGroupItem>
+             <ListGroupItem className="justify-content-between">Fat: <Badge pill>{entry.attributes.fat}</Badge></ListGroupItem>
+             </ListGroup>
+           </div>
+
+         )) : null}
+  */}
+
+  </div>
+
+   )
 }
+
 
 
 export default connect(null, { updateDayEditForm, updateDay })(DayEdit);
